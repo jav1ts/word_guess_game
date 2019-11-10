d686b1ec441ae426bcdebc05541d333db380b3e5
Javits <javits@Joovays-MBP.fios-router.home>
2019-11-10 16:32:31 -0500

    improved functionality of wrong guess row

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,17 @@ $(document).ready(function () {
     let userGuess = "";
     let wrongGuess = 0;
     let correctGuess = 0;
+    let counter = 0;
+    let trashBin = [];
+    let alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+    console.log(alphabet);
+
 
     var movies = ["Terminator", "Titanic", "Lion King", "Rush Hour", "John Wick", "Kill Bill", "Frozen"];
     var movieAnswer = movies[Math.floor(Math.random() * movies.length)].toUpperCase().split('');
     let myBoxes = $('.mystart');
     let wrongKey = $('.badguess');
+
 
     //board setup
     for (let i = 0; i < movieAnswer.length; i++) {
@@ -18,24 +24,39 @@ $(document).ready(function () {
         }
     }
 
-    let answerChecker = (letter, arr) => {
+    let answerChecker = (letter, arr, abcArray) => {
         if (arr.indexOf(letter) >= 0) {
             for (let i = 0; i < arr.length; i++) {
                 if (arr[i] === letter) {
                     myBoxes[i].innerText = `${arr[arr.indexOf(letter)]}`;
                 }
+
             }
         } else {
-            wrongGuess += 1;
-            alert("There is no letter "+userGuess+". You have "+(5-wrongGuess)+" guesses left.");
-            if (wrongGuess >= 5){
-                alert("GAME OVER! You need to watch more movies!");
+            if (abcArray.indexOf(letter)) {
+
+                if (trashBin.indexOf(letter) < 0) {
+                    wrongKey[counter].innerText = letter;
+                    counter++;
+                    trashBin.push(letter);
+
+                }
             }
+            // wrongGuess += 1;
+            // alert("There is no letter "+letter+". You have "+(5-letter)+" guesses left.");
+            // if (wrongGuess >= 5){
+            //     alert("GAME OVER! You need to watch more movies!");
+            // }
         }
     }
 
     $(document).keydown(event => {
-        userGuess = event.key.toUpperCase();
-        answerChecker(userGuess, movieAnswer);
+
+        if (alphabet.indexOf(event.key.toUpperCase()) >= 0) {
+            userGuess = event.key.toUpperCase();
+        }
+
+
+        answerChecker(userGuess, movieAnswer, alphabet);
     });
 });
