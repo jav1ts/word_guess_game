6f47e0b14fbf92588ca80fb96177d35250290699
Javits <javits@Joovays-MBP.fios-router.home>
2019-11-10 19:26:30 -0500

    added class to 3rd row so that I can extend my selection with jQuery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,14 +5,52 @@ $(document).ready(function () {
     let correctGuess = 0;
     let counter = 0;
     let trashBin = [];
-    let alphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
-    console.log(alphabet);
+    let alphabet = "abcdefghijklmnopqrstuvwxyz1234567890".toUpperCase().split("");
 
 
-    var movies = ["Terminator", "Titanic", "Lion King", "Rush Hour", "John Wick", "Kill Bill", "Frozen"];
+
+    var movies = ["Terminator 2", "Titanic", "Lion King", "Rush Hour", "John Wick", "Kill Bill", "Frozen", "Inglourious Basterds", "Rumble in the Bronx"];
     var movieAnswer = movies[Math.floor(Math.random() * movies.length)].toUpperCase().split('');
     let myBoxes = $('.mystart');
     let wrongKey = $('.badguess');
+
+    
+    if (movieAnswer.join("") === "TERMINATOR 2") {
+        alert("Hasta la vista baby");
+    }
+
+    if (movieAnswer.join("") === "TITANIC") {
+        alert("A woman's heart is a deep ocean of secrets.");
+    }
+
+    if (movieAnswer.join("") === "LION KING") {
+        alert("Hakuna Matata!");
+    }
+
+    if (movieAnswer.join("") === "RUSH HOUR") {
+        alert("DO YOU UNDERSTAND THE WORDS THAT ARE COMING OUTTA MY MOUTH?");
+    }
+
+    if (movieAnswer.join("") === "JOHN WICK") {
+        alert("They call him... Baba Yaga.");
+    }
+
+    if (movieAnswer.join("") === "KILL BILL") {
+        alert("That woman deserves her revenge... and we deserve to die.");
+    }
+
+    if (movieAnswer.join("") === "FROZEN") {
+        alert("The cold never bothered me anyway.");
+    }
+
+    if (movieAnswer.join("") === "INGLOURIOUS BASTERDS") {
+        alert("Each and every man under my command owes me one hundred Nazi Scalps.");
+    }
+
+    if (movieAnswer.join("") === "RUMBLE IN THE BRONX") {
+        alert("You don't need diamonds in the bathroom.");
+    }
+
 
 
     //board setup
@@ -35,7 +73,7 @@ $(document).ready(function () {
         } else {
             if (abcArray.indexOf(letter)) {
 
-                if (trashBin.indexOf(letter) < 0) {
+                if (trashBin.indexOf(letter) <= -1) {
                     wrongKey[counter].innerText = letter;
                     counter++;
                     trashBin.push(letter);
@@ -55,8 +93,7 @@ $(document).ready(function () {
         if (alphabet.indexOf(event.key.toUpperCase()) >= 0) {
             userGuess = event.key.toUpperCase();
         }
-
-
+        
         answerChecker(userGuess, movieAnswer, alphabet);
     });
 });
